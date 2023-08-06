396aa71253d64adb85e2b25688b2c0b62ddd74b8
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-08-06 15:52:45 +0100

    fix typo error

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,6 +1,6 @@
 export default function uploadPhoto(filename) {
   const myPromise = new Promise((resolve, reject) => {
-    reject(new Error(`${filename} cannot be procesed`));
+    reject(new Error(`${filename} cannot be processed`));
   });
   return myPromise;
 }
