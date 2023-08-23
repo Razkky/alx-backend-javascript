e3007a0e62d0bd073efa424d410a47d792c7f9b7
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-08-23 15:15:57 +0100

    read from a csv file

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const file = fs.readFileSync(path, 'utf-8');
+    const data = file.split('\n');
+    const body = data.slice(1);
+    const NoOfStudents = body.length;
+    const fields = {};
+    for (let line of body) {
+      line = line.split(',');
+      const course = line[3];
+      const student = line[0];
+      if (fields.hasOwnProperty(course)) {
+	fields[course].push(student);
+      } else {
+	fields[course] = [];
+	fields[course].push(student);
+      }
+    }
+    console.log(`Number of students: ${NoOfStudents}`);
+    for (const field in fields) {
+      if (fields) {
+	console.log(`Number of students in ${field}: field.length. List: ${fields[field].toString().split(',').join(', ')}`);
+      }
+    }
+  } catch (err) {
+    throw new Error('Cannot laod the database');
+  }
+}
+module.exports = countStudents;
