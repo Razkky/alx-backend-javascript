1a288571a4116491c707bec9120473d60f64b5dd
Abdul-Razak Muhammad Jamiu <muhammadjamiuabdulrazak@gmail.com>
2023-09-07 09:59:35 +0100

    create an array of classRooms

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,14 @@
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  const sizes = [19, 20, 34];
+  const classRooms = [];
+
+  for (const size of sizes) {
+    const classRoom = new ClassRoom(size);
+    classRooms.push(classRoom);
+  }
+  console.log(classRooms);
+}
+
+initializeRooms();
