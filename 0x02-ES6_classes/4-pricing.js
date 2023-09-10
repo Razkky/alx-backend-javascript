318a895905efb8739482b9d70abda667e0053cb5
Abdul-Razak Muhammad Jamiu <muhammadjamiuabdulrazak@gmail.com>
2023-09-10 09:51:28 +0100

    complete 4th task

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,33 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  set currency(currency) {
+    if (!(currency instanceof Currency)) throw TypeError('currency must be an instance of Currency');
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this._currency.name} ${this._currency.code}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
