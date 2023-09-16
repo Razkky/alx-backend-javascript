7e2b770190d5551d74fd3f8b5d0f874b80ea487b
Abdul-Razak Muhammad Jamiu <muhammadjamiuabdulrazak@gmail.com>
2023-09-16 19:08:25 +0100

    complete task 4

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,11 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const grade = newGrades.filter((newGrade) => newGrade.studentId === student.id);
+      const newStudent = student;
+      if (grade[0]) newStudent.grade = grade[0].grade;
+      else newStudent.grade = 'N/A';
+      return newStudent;
+    });
+}
