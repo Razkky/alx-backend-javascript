bf1eb4b68750f76c7dc6deef96c76c94bc3af8a1
Abdul-Razak Muhammad Jamiu <muhammadjamiuabdulrazak@gmail.com>
2023-09-27 20:58:19 +0100

    complete task 9a

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,12 @@
+export default function guardrail(mathFunction) {
+  const newArray = [];
+
+  try {
+    newArray.push(mathFunction());
+  } catch (err) {
+    newArray.push(err.toString());
+  } finally {
+    newArray.push('Guardrail was processed');
+  }
+  return newArray;
+}
